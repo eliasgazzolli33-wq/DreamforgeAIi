10bfc47b86a1ef1b5bf6b11cd2e4834c5f63b8d8
eliasgazzolli33-wq <eliasgazzolli33@gmail.com>
2025-11-06 03:15:58 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🚀 DreamForgeAI</h1>
+      <p>Seu site está funcionando com Next.js!</p>
+    </div>
+  );
+}
