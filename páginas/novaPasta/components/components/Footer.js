1fd3e94d089db7c3818832ae8a55d3a6acc6066b
eliasgazzolli33-wq <eliasgazzolli33@gmail.com>
2025-11-07 11:33:47 -0300

    Create Footer.js

---
+++ páginas/novaPasta/components/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{padding: "20px", background: "#111", color: "#fff", marginTop: "40px"}}>
+      <p>© 2025 DreamForge AI - Todos os direitos reservados</p>
+    </footer>
+  );
+}
