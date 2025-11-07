392539c5bd07f5c48b2069f201234f116359a7e9
eliasgazzolli33-wq <eliasgazzolli33@gmail.com>
2025-11-07 11:41:26 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>DreamForge AI</h1>
+      <p>Seu site está funcionando!</p>
+    </div>
+  );
+}
