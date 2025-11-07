e1938af593e8ad036c470f5840b1dbcc9ae838ff
eliasgazzolli33-wq <eliasgazzolli33@gmail.com>
2025-11-07 15:55:31 -0300

    Create page.js

---
+++ app/erro/page.js
@@ -0,0 +1,3 @@
+export default function Erro() {
+  return <div className="p-12 text-center"><h1 className="text-2xl font-bold">Payment Failed</h1><p>Please try again or contact support.</p></div>;
+}
