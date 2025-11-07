bfa49f3993932b499df0a020592704cb5f4936b9
eliasgazzolli33-wq <eliasgazzolli33@gmail.com>
2025-11-07 10:24:32 -0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-      <h1>DreamForgeAI</h1>
-      <p>Site em construção... 🚀</p>
-    </div>
-  );
-}
