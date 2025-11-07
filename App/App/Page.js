9a19f0d95b693140b6ff37320c0fa61b780a94e3
eliasgazzolli33-wq <eliasgazzolli33@gmail.com>
2025-11-07 12:00:38 -0300

    Create Page.js

---
+++ App/App/Page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Dreamforge AI</h1>
+      <p>Seu projeto Next.js está funcionando!</p>
+    </main>
+  );
+}
